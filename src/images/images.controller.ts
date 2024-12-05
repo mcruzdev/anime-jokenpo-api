c06ff971d3d081aaaad0c545a7e9ed2cc62013fd
Matheus Cruz <matheuscruz.dev@gmail.com>
2024-12-04 22:07:07 -0300

    remove swagger docs

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,9 +1,7 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
 import { Response } from 'express';
 
 @Controller('images')
-@ApiBearerAuth()
 export class ImagesController {
   @Get(':image')
   downloadImage(@Res() res: Response, @Param('image') image: string) {
